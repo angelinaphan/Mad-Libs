26bb1f85987567a9c0f2084b2207586b9f9e6f9c
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2024-04-18 10:25:36 -0400

    colors | story | padding

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var yourName = prompt("Enter your name: ");
-var occupation = prompt("Enter an occupation: ");
-var salary = prompt("Enter a number between 2 and 100: ");
-var yourExp = prompt("Enter a saying or expression: ");
-var verb = prompt("Enter an action verb not ending in ING: ");
+var pluralNoun = prompt("Enter a plural noun food: ");
+var verb = prompt("Enter a verb: ");
+var noun = prompt("Enter a noun: ");
+var color = prompt("Enter a color: ");
+var colors = prompt("Enter a different color: ");
+var nouns = prompt("Enter a noun: ");
 var adjective = prompt("Enter an adjective: ");
-var pluralNoun = prompt("Enter a plural noun: ");
+var nounss = prompt("Enter a noun: ");
+var adverb = prompt("Enter an adverb: ");
+var animal = prompt("Enter an animal: ");
+var pluralNouns = prompt("Enter a plural noun: ");
+var pluralnounss = prompt("Enter a plural noun: ");
+var pluralnounsss = prompt("Enter a plural noun: ");
 
-document.write(yourName + " just got offered a new job as a " + occupation + " making " + salary + " dollar per hour.'" + yourExp + "' you say! Your job requires you to " + verb + " and make " + adjective + " " + pluralNoun + ".");
+// document.write(yourName + " just got offered a new job as a " + occupation + " making " + salary + " dollar per hour. '" + yourExp + "' you say! Your job requires you to " + verb + " and make " + adjective + " " + pluralNoun + ".");
+
+document.write(" The night before Easter, we boil " + pluralNoun + " and " + verb + " them different colors with " + noun + " . " + " Easter baskets come in bright colors like " + color + colors + " and are about the size of a " + nouns + " . " + " We fill our baskets with " + adjective + nounss + " to hold our treats and wait " + adverb + " for morning" + " . " + " Then we wake up to see if the Easter " + animal + " left any treats in our Easter " + pluralNouns + " , " + " like chocolate " + pluralnounss + " or marshmallow " + pluralnounsss + " . ");
